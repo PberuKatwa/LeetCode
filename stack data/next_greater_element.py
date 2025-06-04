8e740403c6587d56d46e89d3df6532f6f0170b26
PberuKatwa <pberu8531@gmail.com.com>
2025-06-04 18:52:12 +0300

    next greater element v1

--- stack data/next_greater_element.py
+++ stack data/next_greater_element.py
@@ -5,3 +5,26 @@
 # Input: [4, 5, 2, 25]
 # Output: [5, 25, 25, -1]
 
+def next_greater_element(array:list):
+
+    greatest = 0
+    output = [-1] * len(array)
+    stack = []
+
+    for i in range( len( array ) ):
+
+        if stack:
+            print("indexxx", i, "stackkk", array[ stack[-1] ], "arr",  array[i], "outputt", output)
+
+        while stack and array[ stack[-1] ] < array[i]:
+
+            popped_index = stack.pop()
+            output[popped_index] = array[i]
+            print(f"Popped {popped_index}, updated output[{popped_index}] = {array[i]}, outt {output}")
+
+        stack.append(i)  
+        print(f"Stack after processing index {i}: {stack}")
+
+    return output
+
+print( next_greater_element( [4,5,2,25] ) )
