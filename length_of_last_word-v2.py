804dedab8a2f6efed1e128504192072851fa943f
PberuKatwa <pberu8531@gmail.com.com>
2025-07-15 12:17:57 +0300

    length of last word v2:final draft

--- length_of_last_word-v2.py
+++ length_of_last_word-v2.py
@@ -20,31 +20,21 @@
 
 def length_last_word(s:str):
 
-    total_length = len(s)
-    n = 0
+    n = len(s) - 1
     t = 0
-    found = False
-    found_occurrence = 0
 
-    while ( found_occurrence < 1 ) and ( abs(n) < total_length ) :
-        print("nnnn", n, total_length)
+    while ( n > 0 ) and ( s[n] == " " ) :
         n = n - 1
 
-        if ( s[n] != " " ):
-            t = t + 1 
-
-        if ( t > 1 ) and ( s[n] == " " ):
-            found_occurrence = found_occurrence + 1
-
-        
-
-
+    while ( n > 0 ) and ( s[n] != " " ):
+        t = t + 1
+        n = n - 1
 
     return t
 
 
 
-# print( length_last_word("   fly me   to   the moon  ") )
+print( length_last_word("   fly me   to   the moon  ") )
 # print( length_last_word("luffy is still joyboy") )
 # print( length_last_word("y ") )
-print( length_last_word(" abc ") )
+# print( length_last_word(" abc   ") )
